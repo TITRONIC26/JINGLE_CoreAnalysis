5c47b871713a54244bb05651899da8527a54b64b
Jestyn S <jestyn.strik@mesomat.com>
2023-08-22 09:54:08 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Main function. Run this code to access the data from the local JINGLE csv files.
+This code is meant to give a "quick" look at the data within the JINGLE files, showing brief a brief overlook at the main information.
+"""
+
+#import libraries here
+
+
+#set-up parameters and globals here
+
+
+#main function for analyzing data
+def main():
+    print("Hello World")
+    return
+
+#call on the main function when the script is executed
+if __name__ == '__main__':
+    main()
+
