2c7f2dcf5cd335a2eafffddd29981c15fd506776
Jestyn S <jestyn.strik@mesomat.com>
2023-08-22 11:01:36 -0400

    Created gather_data.py

---
+++ gather_data.py
@@ -0,0 +1,12 @@
+"""
+Code used explicitly for collecting and processing the data stored locally
+"""
+
+#import libraries here
+import os
+import pandas as pd
+
+#global variables and parameters here
+
+
+#functions go here
