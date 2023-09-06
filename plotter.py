eaa951ae8827a85349590f22adf3a41b7a892aa5
Jestyn S <jestyn.strik@mesomat.com>
2023-09-06 10:27:34 -0400

    Added new plotting script

---
+++ plotter.py
@@ -0,0 +1,25 @@
+"""
+New plotting script for plotting the data gathered from the 4 new datafiles
+"""
+
+#libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+#old plotting scripts
+import base_plotter as BPLT
+import ratio_plots as RPLT
+
+#essential scripts
+import gather_data as GD
+import callable_functions as CF
+import constants as C
+
+#data analysis and manipulation
+import core_analysis as CA
+import data_manipulation as DM
+
+#formatting
+import formatting_functions as FF
+
+#define functions here
